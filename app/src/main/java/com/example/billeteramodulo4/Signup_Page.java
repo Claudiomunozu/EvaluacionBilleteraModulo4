194c02caba9ff5d4e21ab6d23db4c7ba71172f66
Claudiomunozu <claudio.munoz.ucuentas@gmail.com>
2024-04-23 20:40:50 -0400

    prueba actualizar proyecto

--- app/src/main/java/com/example/billeteramodulo4/Signup_Page.java
+++ app/src/main/java/com/example/billeteramodulo4/Signup_Page.java
@@ -1,24 +1,33 @@
 package com.example.billeteramodulo4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class Signup_Page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.signup_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Abrir nueva activity
+
+        ImageButton boton3 = findViewById(R.id.sendlogin);
+
+        boton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ir a activity 4
+                Intent i = new Intent(Signup_Page.this, Home_Page.class);
+                startActivity(i);
+            }
         });
+
     }
 }
